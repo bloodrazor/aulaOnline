92b4fcb7fd352866a9d1422ee7960205b3dbf6db
Jessica <bloodrazor@users.noreply.github.com>
2020-11-13 11:08:17 -0300

    Aulas

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+//codigo não executavel
+
+//procedural
+processsamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+    
+}
+objeto.processamento() // foco passou a ser objeto
+
+//principios importante
+// 1. abstração
+// 2. encapsulamento
+// 3. herança(prototype)
+// 4. 
